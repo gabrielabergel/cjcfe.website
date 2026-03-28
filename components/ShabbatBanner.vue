b020a4ee0c015f7163d91f21e9817ff1e980d102
gabrielabergel <44164440+gabrielabergel@users.noreply.github.com>
2026-03-28 22:05:14 +0100

    Update ShabbatBanner.vue

--- components/ShabbatBanner.vue
+++ components/ShabbatBanner.vue
@@ -261,7 +261,7 @@ const formatFeteDate = (date: string | null | undefined): string => {
     .shabbat-banner_toggle {
       display: flex;
       position: fixed;
-      top: calc(100dvh - 140px);
+      top: calc(100svh - 140px);
       left: 20px;
       z-index: 1000;
       width: 56px;
